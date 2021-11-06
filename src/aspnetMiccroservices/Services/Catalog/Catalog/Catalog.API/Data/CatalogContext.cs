4af1ef4821572afed40356d1e29228da94e82ec8
Mohammed <Mohammed@DESKTOP-FUFA0D9>
2021-11-06 14:11:17 +0200

    _2

---
+++ src/aspnetMiccroservices/Services/Catalog/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entitties;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContexts : ICatalogContext
+    {
+
+        public CatalogContexts(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            var Products = database.GetCollection< Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/aspnetMiccroservices/Services/Catalog/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.API.Entitties;
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+         IMongoCollection<Product> Products { get; }
+    }
+}
